a8432e75bab4f3ca2e2f0ad1b74e10d8ebbdfee1
batslapper43 <juanmagana00@gmail.com>
2025-12-03 17:29:21 -0800

    javascript
    link

--- media-script.js
+++ media-script.js
@@ -2,7 +2,7 @@
 
 // function for our list view
 async function getAllRecords() {
-  let getResultElement = document.getElementById("");
+  let getResultElement = document.getElementById("journalist");
 
   const options = {
     method: "GET",
@@ -18,7 +18,48 @@ async function getAllRecords() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data); // response is an object w/ .records array
+   getResultElement.innerHTML = ""; // clear brews
+
+      let newHtml = "";
+
+      for (let i = 0; i < data.records.length; i++) {
+        let logo = data.records[i].fields["Logo"]; // here we are getting column values
+        let name = data.records[i].fields["Name"]; //here we are using the Field ID to fecth the name property
+        let neighborhood = data.records[i].fields["Neighborhood"];
+
+        newHtml += `
+        
+         <div class="col-xl-4 cardImageText">
+          <div class="card list move border-dark mb-5" style="width: 20rem;">
+          <a href="breweries.html?id=${data.records[i].id}">${
+          logo
+            ? `<img class="card-img-top rounded" alt="${name}" src="${logo[0].url}">`
+            : ``
+        }
+          </a>
+          <p hidden class="card-key">${neighborhood}</p>
+          </div>
+          </div>
+        </div>
+    
+        
+        `;
+      }
+
+      getResultElement.innerHTML = newHtml;
     });
 }
 
-getAllRecords();
+// look up window.location.search and split, so this would take
+// https://dmspr2021-airtable-app.glitch.me/index.html?id=receHhOzntTGZ44I5
+// and look at the ?id=receHhOzntTGZ44I5 part, then split that into an array
+// ["?id=", "receHhOzntTGZ44I5"] and then we only choose the second one
+let idParams = window.location.search.split("?id=");
+if (idParams.length >= 2) {
+  
+  getOneRecord(idParams[1]); // create detail view HTML w/ our id
+} else {
+  
+  
+  getAllRecords(); // no id given, fetch summaries
+}
